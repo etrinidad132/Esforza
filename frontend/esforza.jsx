77aee0d0e2a58ea14ecc46e904f590fd295abc1e
etrinidad <edward.trinidad132@gmail.com>
2020-04-06 16:49:03 -0400

    React is now up and running

---
+++ frontend/esforza.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import configureStore from "./store/store"
+import Root from "./components/root"
+
+///////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root")
+    ReactDOM.render(<Root/>, root)
+})
