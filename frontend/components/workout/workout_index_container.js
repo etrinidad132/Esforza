ff85ffa6d30c526aaa16187c7e59190e0e5e9c6e
etrinidad <edward.trinidad132@gmail.com>
2020-05-28 15:44:31 -0400

    started workout index

--- frontend/components/workout/workout_index_container.js
+++ frontend/components/workout/workout_index_container.js
@@ -1,26 +1,27 @@
 import { connect } from "react-redux";
-import IndexWorkout from "./index_workout";
+import WorkoutIndex from "./workout_index";
 import { fetchWorkouts, destroyWorkout } from "../../actions/workout_actions";
 
 const mapStateToProps = (state) => {
   const { workouts } = state.entities;
-  const { id } = state.session.currentUser;
+//   const { id } = state.session.currentUser;
   return {
-    loading: state.ui.loading.indexLoading,
+    // loading: state.ui.loading.indexLoading,
     workouts: Object.values(workouts)
       .reverse()
-      .filter((workout) => id === workout.userId)
-      .sort((a, b) => (b.createDate > a.createDate ? 1 : -1)),
+      .filter((workout) => state.session.id === workout.user_id)
+      //   .sort((a, b) => (b.create_date > a.create_date ? 1 : -1)),
+      .sort((a, b) => Math.sign(a.create_date - b.create_date)),
     // recentWorkouts: Object.values(workouts)
     //   .reverse()
     //   .filter((workout) => id === workout.userId)
-    //   .sort((a, b) => (b.createDate > a.createDate ? 1 : -1)),
+    //   .sort((a, b) => (b.create_date > a.create_date ? 1 : -1)),
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
   fetchWorkouts: () => dispatch(fetchWorkouts()),
-  deleteWorkout: (id) => dispatch(destroyWorkout(id)),
+  deleteWorkout: (workoutId) => dispatch(destroyWorkout(workoutId)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(IndexWorkout);
+export default connect(mapStateToProps, mapDispatchToProps)(WorkoutIndex);
